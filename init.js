0a1111c135e7307fcd3b96652061b41cc6f54d52
nikhiljha <nikhilfromirvine@gmail.com>
2016-06-30 12:48:17 -0700

    Initial Commit
    - Does not work under any OS apparently...
    - Does something, so that's cool!

---
+++ init.js
@@ -0,0 +1,53 @@
+// We are based on electron, so we need to initiate it.
+const electron = require('electron');
+const app = require('app');
+const BrowserWindow = require('browser-window')
+var system = "Unknown OS";
+var os = require('os')
+
+switch(os.type()) {
+    case "Linux":
+        system = "Linux";
+        break;
+    case "Darwin":
+        system = "Mac";
+        break;
+    case "Windows_NT":
+        system = "Windows";
+        break;
+    default:
+        system = "Unknown OS";
+}
+
+// When the app is ready...
+app.on('ready', function () {
+
+  var express = require('express');
+  var path = require('path');
+  var app = express();
+
+  // Define the port to run on
+  app.set('port', 3000);
+
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use('/lightwallet/wallet/*', express.static(path.join(__dirname + '/public')));
+  app.use('/lightwallet/login', express.static(path.join(__dirname + '/public')));
+  app.use('/lightwallet', express.static(path.join(__dirname + '/public')));
+
+  // Listen for requests
+  var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+  });
+
+  // Create a new window and let it load in the background.
+  var win = new BrowserWindow({width: 800, height: 600, show: false, title: "NEM Wallet for " + system});
+
+  // When it is closed, delete the window.
+  win.on('closed', function() {
+    win = null;
+  });
+
+  win.loadURL('http://localhost:3000');
+  win.show();
+})
